302fbcaae034ff0578cecd0ae27cc906727f6589
Khairul-hub <64126807+Khairul-hub@users.noreply.github.com>
2020-06-19 12:57:52 +0800

    Added Unity Project

---
+++ 14_PA11_Khairul/Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    private int score;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Score : " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        // initially, the temporary vector should equal the player's position
+        Vector3 clampedPosition = transform.position;
+        // Now we can manipulte it to clamp the y element
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, -4f, 4f);
+        // re-assigning the transform's position will clamp it
+        transform.position = clampedPosition;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            score++;
+            scoreText.text = "Score : " + score;
+        }
+    }
+}
